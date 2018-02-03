722f6be659f10a7682b6979c4825293bfe500894
Christian <ChrisWcs@users.noreply.github.com>
2018-02-03 00:21:44 -0700

    Filled in reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -14,8 +14,18 @@ export default reducer = (state, action) => {
                 students: [...state.student, action.student]
             };
         case MARK_DAY:
+            const {dayIndex, studentIndex, studentBool} = action;
             return {
-
+                ...state,
+                attendance: [
+                    ...state.attendance.slice(0, dayIndex),
+                    [
+                        ...state.attendance[dayIndex].slice(0, studentIndex),
+                        studentBool,
+                        ...state.attendance[dayIndex].slice(studentIndex + 1),
+                    ],
+                    ...state.attendance.slice(dayIndex + 1),
+                ],
             };
         default:
             return state;
